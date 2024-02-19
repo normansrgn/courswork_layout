1e36adca259157e583a9df0b96cea20f1c1eb18b
normansrgn <145141686+normansrgn@users.noreply.github.com>
2024-02-19 17:29:18 +0300

    add_burger_menu

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ const swiper = new Swiper('.swiper', {
     //     disableOnInteraction: false,
     // },
 });
+
+const burgerMenu = () => {
+    const burgerBtn = document.querySelector('#burger__btn');
+    const burgerContainer = document.querySelector('.burger__container')
+    const burgerSpan = document.querySelector('#burgerSpan');
+
+    burgerBtn.addEventListener('click', (e) => {
+        burgerContainer.classList.toggle('open');
+        burgerSpan.classList.toggle('btn-open-animation');
+    });
+
+    burgerContainer.addEventListener('click', e => {
+        if (!e.target.classList.contains('burger__content')) {
+            burgerContainer.classList.remove('open');
+            burgerSpan.classList.remove('btn-open-animation');
+        }
+    })
+}
+
+burgerMenu();
+
