3a7ec4be154cc6710b681195ed6d9d4010654e0c
normansrgn <145141686+normansrgn@users.noreply.github.com>
2024-03-05 14:27:12 +0300

    ;

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 
 // SWIPER //
 
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters   
+//     loop: true,
+
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.first',
+//         prevEl: '.back',
+//     },
+
+   
+// });
+
+
 const swiper = new Swiper('.swiper', {
     // Optional parameters
+
     loop: true,
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.first',
-        prevEl: '.back',
-    },
 
     // autoplay: {
     //     delay: 2500,
     //     disableOnInteraction: false,
     // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+   
 });
 
 const burgerMenu = () => {
