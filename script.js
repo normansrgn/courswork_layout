bba9fb260b9e64b0e1a90c284709d6fc04895db7
normansrgn <normanmagic2004@gmail.com>
2024-03-08 20:40:02 +0300

    $

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// BURGER MENU //
+
+const burgerMenu = () => {
+    const burgerBtn = document.querySelector('#burger__btn');
+    const burgerContainer = document.querySelector('.burger__container')
+    const burgerSpan = document.querySelector('#burgerSpan');
+
+    burgerBtn.addEventListener('click', (e) => {
+        burgerContainer.classList.toggle('open');   
+        burgerSpan.classList.toggle('btn-open-animation');
+    });
+
+    burgerContainer.addEventListener('click', e => {
+        if (!e.target.classList.contains('burger__content')) {
+            burgerContainer.classList.remove('open');
+            burgerSpan.classList.remove('btn-open-animation');
+        }
+    })
+}
+
+burgerMenu();
+
 
 // SWIPER shop //
 const swiper = new Swiper('.swiper', {
@@ -38,23 +60,5 @@ const swiper_profile = new Swiper('.swiper_profile', {
     },
 });
 
-const burgerMenu = () => {
-    const burgerBtn = document.querySelector('#burger__btn');
-    const burgerContainer = document.querySelector('.burger__container')
-    const burgerSpan = document.querySelector('#burgerSpan');
 
-    burgerBtn.addEventListener('click', (e) => {
-        burgerContainer.classList.toggle('open');   
-        burgerSpan.classList.toggle('btn-open-animation');
-    });
-
-    burgerContainer.addEventListener('click', e => {
-        if (!e.target.classList.contains('burger__content')) {
-            burgerContainer.classList.remove('open');
-            burgerSpan.classList.remove('btn-open-animation');
-        }
-    })
-}
-
-burgerMenu();
 
