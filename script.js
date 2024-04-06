b92b20d31c21cd9df8a1db3fceaebdc74cf8adb3
normansrgn <normanmagic2004@gmail.com>
2024-04-06 17:34:58 +0300

    fix search

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// ПОИСК ТОВАРОВ //
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchinput");
+    const cards = document.querySelectorAll(".cataog_card");
+
+    searchInput.addEventListener("input", function () {
+        const searchTerm = searchInput.value.trim().toLowerCase();
+
+        cards.forEach(function (card) {
+            const cardname = card.querySelector('.cardname').textContent.trim().toLowerCase();
+            if (cardname.includes(searchTerm)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        });
+    });
+});
+
 // BURGER MENU //
 
 const burgerMenu = () => {
