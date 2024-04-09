49f5c643641e91b097873b86d773571496a27536
normansrgn <145141686+normansrgn@users.noreply.github.com>
2024-04-09 11:52:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Получаем все элементы с классом "size_item"
+const sizeItems = document.querySelectorAll('.size_item');
+
+function addActiveClass() {
+    sizeItems.forEach(item => {
+        if (!this.classList.contains('activesize')) {
+            item.classList.remove('activesize');
+        }
+    });
+
+    this.classList.toggle('activesize');
+}
+
+sizeItems.forEach(item => {
+    item.addEventListener('click', addActiveClass);
+});
+
 
 // BURGER MENU //
 
@@ -20,6 +37,8 @@ const burgerMenu = () => {
 }
 
 burgerMenu();
+
+
 
 
 // // SWIPER shop //
