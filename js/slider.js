ef1b01e32537d36de072e752e84822117f9a43e8
normansrgn <normanmagic2004@gmail.com>
2024-05-02 20:01:56 +0300

    fix slider

--- js/slider.js
+++ js/slider.js
@@ -6,10 +6,10 @@ const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
 
     // Autoplay
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
 
     // Navigation arrows
     navigation: {
@@ -18,54 +18,8 @@ const swiper = new Swiper('.swiper', {
     },
 });
 
-// SWIPER PROFILE
 
-const swiper_profile = new Swiper('.swiper_profile', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 8,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.fa-arrow-left',
-        prevEl: '.fa-arrow-right',
-    },
-
-    breakpoints:  {
-        // when window width is >= 320px
-        320: {
-          slidesPerView: 2,
-        //   spaceBetween: 
-        },
-
-        350: {
-            slidesPerView: 1,
-            spaceBetween: 0
-        },
-
-        780: {
-            slidesPerView: 2,
-            spaceBetween:20
-        },
-
-        920 : {
-            slidesPerView: 2,
-            spaceBetween: 0
-        },
-
-        // when window width is >= 480px
-        1187: {
-          slidesPerView: 3,
-          spaceBetween: 30
-        },
-        // when window width is >= 640px
-        1700: {
-          slidesPerView: 3,
-          spaceBetween: 40
-        }
-      }
-});
 
 
 //SHOP TAB SCRIPT // 
